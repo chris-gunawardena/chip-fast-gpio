6dbeba6091e24ce8d84d4d176d0eb1025fb33ef4
Chris Gunawardena <chris.gun@nl.ibm.com>
2016-12-05 13:57:58 +0100

    moved export/set mode to own functions

--- chip-fast-gpio.cc
+++ chip-fast-gpio.cc
@@ -47,46 +47,62 @@ const char* join(const char* str1, const char* str2, const char* str3) {
   return joint_str;
 }
 
-void GPIOwrite(const v8::FunctionCallbackInfo<v8::Value>& args)
-{
-	v8::Isolate* isolate = args.GetIsolate();
-	v8::HandleScope scope(isolate);
-
+void gpio_export(const v8::FunctionCallbackInfo<v8::Value>& args)
+{	// args: pin
     v8::String::Utf8Value pinUtf8(args[0]->ToString());
 	const char* pin = ToCString(pinUtf8);
-	//printf("pin: %s\n", pin);
-
-    v8::String::Utf8Value valueUtf8(args[1]->ToString());
-	const char* value = ToCString(valueUtf8);
-	//printf("value: %s\n", value);
-
 
 	// export pin
 	int export_fd = open("/sys/class/gpio/export", O_WRONLY);  ECHK(export_fd != -1);
 	/* If port 5 is already exported, the following will fail. That's OK, but warn. */
 	int wlen = write(export_fd, pin, 4);  WCHK(wlen == 4);
-	close(export_fd);	
+	close(export_fd);
+}
 
-    // set for output
-    //printf(join("/sys/class/gpio/gpio", pin, "/direction"));
+void gpio_mode(const v8::FunctionCallbackInfo<v8::Value>& args)
+{	// args: pin, mode
+    v8::String::Utf8Value pinUtf8(args[0]->ToString());
+	const char* pin = ToCString(pinUtf8);
+    v8::String::Utf8Value modeUtf8(args[0]->ToString());
+	const char* mode = ToCString(modeUtf8);
+
+    // set pin for input/output
 	int dir_fd = open(join("/sys/class/gpio/gpio", pin, "/direction"), O_RDWR);  ECHK(dir_fd != -1);
-	wlen = write(dir_fd, "out", 3);  ECHK(wlen == 3);
+	int wlen = write(dir_fd, "out", 3);  ECHK(wlen == 3);
 	close(dir_fd);
+}
+
+void gpio_write(const v8::FunctionCallbackInfo<v8::Value>& args)
+{	// args: pin, value
+    v8::String::Utf8Value pinUtf8(args[0]->ToString());
+	const char* pin = ToCString(pinUtf8);
+    v8::String::Utf8Value valueUtf8(args[1]->ToString());
+	const char* value = ToCString(valueUtf8);
 
 	// // write
     int out_val_fd = open(join("/sys/class/gpio/gpio", pin, "/value"), O_RDWR);  ECHK(out_val_fd != -1);
-	wlen = write(out_val_fd, value, 2);  ECHK(wlen == 2);
+	int wlen = write(out_val_fd, value, 2);  ECHK(wlen == 2);
 	close(out_val_fd);
+}
 
-	// unexport
-	export_fd = open("/sys/class/gpio/unexport", O_WRONLY);  ECHK(export_fd != -1);
-	wlen = write(export_fd, pin, 4);  ECHK(wlen == 4);
+void gpio_unexport(const v8::FunctionCallbackInfo<v8::Value>& args)
+{	// args: pin
+    v8::String::Utf8Value pinUtf8(args[0]->ToString());
+	const char* pin = ToCString(pinUtf8);
+
+	// unexport pin
+	int export_fd = open("/sys/class/gpio/unexport", O_WRONLY);  ECHK(export_fd != -1);
+	int wlen = write(export_fd, pin, 4);  ECHK(wlen == 4);
 	close(export_fd);
 }
 
 
+
 void init(v8::Local<v8::Object> exports) {
-  NODE_SET_METHOD(exports, "write", GPIOwrite);
+  NODE_SET_METHOD(exports, "export", gpio_export);
+  NODE_SET_METHOD(exports, "mode", gpio_mode);
+  NODE_SET_METHOD(exports, "write", gpio_write);
+  NODE_SET_METHOD(exports, "unexport", gpio_unexport);
 }
 
 NODE_MODULE(binding, init);
--- index.js
+++ index.js
@@ -4,10 +4,15 @@
 var bindings = require('bindings');
 var gpio = bindings('chip-fast-gpio');
 
-  var start_time = new Date().getTime();
-  for(var i=0; i<1000; i++) {
-    gpio.write(408, 1)
-  }
-  var end_time = new Date().getTime();
-  console.log(end_time - start_time);
+gpio.export(132);
+gpio.mode(132, "out");
 
+// https://bbs.nextthing.co/t/solved-gpio-pin-numbers/2931/16
+var start_time = new Date().getTime();
+for(var i=0; i<1000; i++) {
+	gpio.write(132, 1);
+}
+var end_time = new Date().getTime();
+console.log(end_time - start_time);
+
+gpio.unexport(132);
